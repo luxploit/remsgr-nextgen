1bad764e30ce8fa4d9e41754fb25b57892d657a5
paris <87091573+herbodytea@users.noreply.github.com>
2024-07-28 17:14:47 +0200

    idk

--- handlers/switchboard/MSG.js
+++ handlers/switchboard/MSG.js
@@ -1,7 +1,6 @@
 const chalk = require('chalk');
 const { verifyJWT } = require("../../utils/auth.util");
 const { getAllParticipantsSockets } = require("../../utils/sb.util");
-// const MimeParser = require("../../utils/parsers/mime.util");
 const mailparser = require('mailparser');
 
 module.exports = async (socket, args, command, data) => {
@@ -23,29 +22,11 @@ module.exports = async (socket, args, command, data) => {
 
     const email = socket.passport;
 
-    // parse the full payload
-    const parsed = await mailparser.simpleParser(fullPayload);
-
-    // console log the parsed email
-    console.log(parsed);
+    // const parsed = await mailparser.simpleParser(fullPayload);
+    // console.log(parsed);
 
     const allSockets = getAllParticipantsSockets(socket.chat, email);
 
-    //if (parser.getHeader('TypingUser')) {
-    //    allSockets.forEach(s => {
-    //        s.write(`MSG ${socket.passport} ${socket.friendly_name} ${messageTotal}\r\n${payload}`);
-    //    });
-    //    return;
-    //}
-//
-    //if (parser.getHeader('Content-Type') == "text/plain; charset=UTF-8") {
-    //    allSockets.forEach(s => {
-    //        s.write(`MSG ${socket.passport} ${socket.friendly_name} ${messageTotal}\r\n${payload}`);
-    //    });
-    //    return;
-    //}
-
-    // get length of message
     const messageTotal = Buffer.byteLength(payload, 'utf8');
 
     try {
