09ba6715d88fa7c3c5874b71cb4d2b7f4cf526a2
paris <87091573+herbodytea@users.noreply.github.com>
2024-09-11 14:49:34 +0200

    idk anymore

--- data/versions.js
+++ data/versions.js
@@ -1,16 +1,17 @@
 const supportedVersions = [
+//    'MSNP14',
     'MSNP13',
     'MSNP12',
     'MSNP11',
     'MSNP10',
-//    'MSNP9', // Introduces display pictures, and a bunch of other stuff, see on NINA Wiki
-//    'MSNP8',
-//    'MSNP7',
-//    'MSNP6',
+    'MSNP9', 
+    'MSNP8', // Introduces display pictures, and a bunch of other stuff, see on NINA Wiki 
+    'MSNP7',
+    'MSNP6',
     'MSNP5',
     'MSNP4',
-//    'MSNP3',
-//    'MSNP2'
+    'MSNP3',
+    'MSNP2'
 ];
 
 module.exports = supportedVersions;
--- handlers/UUX.js
+++ handlers/UUX.js
@@ -17,16 +17,22 @@ module.exports = async (socket, args, command) => {
 
     // get payload from the command (get after first \r\n)
     const payload = command.split('\r\n').slice(1).join('\r\n');
-    const parsed = parser.parse(payload);
+    const payloadXML = payload.slice(0, payload.lastIndexOf('>') + 1);
+
+    console.log(payloadXML);
+
+    const parsed = parser.parse(payloadXML);
+
+    console.log(parsed);
 
     if (parsed.Data.PSM === undefined || parsed.Data.CurrentMedia === undefined) {
         console.log(`${chalk.red.bold('[UUX]')} ${socket.passport} has sent an invalid payload.`);
         return;
     }
 
-    socket.customStatus = payload;
+    socket.customStatus = payloadXML;
 
-    const payloadLength = Buffer.byteLength(payload, 'utf8');
+    const payloadLength = Buffer.byteLength(payloadXML, 'utf8');
 
     // Send acknowledgement
     socket.write(`UUX ${transactionID} 0\r\n`);
@@ -45,7 +51,7 @@ module.exports = async (socket, args, command) => {
             }
 
             if (contactSocket) {
-                contactSocket.write(`UBX ${socket.passport} ${payloadLength}\r\n${payload}`);
+                contactSocket.write(`UBX ${socket.passport} ${payloadLength}\r\n${payloadXML}`);
             }
         }
     });
