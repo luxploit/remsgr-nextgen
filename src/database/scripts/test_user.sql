5585b6a773dd30aec4824f25c7ac67a3e27a4cb0
Laura Ava Streisky <laura@luxploit.net>
2024-10-03 00:59:37 +0200

    added test user script

---
+++ src/database/scripts/test_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO "accounts" ("uid", "guid", "screen_name", "email", "password_md5", "password_sha224", "is_verified", "last_modified") VALUES (2, 'd39f5379-a980-4f08-b153-bb5fde7bc20b', 'kurwa', 'kurwa@remsgr.net', 'e3c2c0d23848bddc3db9a45f41e31994', '""', 'true', '2024-10-03 00:01:53');
+-- kurwa:boberkurwa
